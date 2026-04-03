33ef33a331cf1e911168a41f3a44499ac4cafd45
Gerald <ezemagerald941@gmail.com>
2026-04-04 00:42:04 +0100

    Add function

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import os
+import time
+from telegram import Bot
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = Bot(token=TOKEN)
+
+running = True
+
+while running:
+    bot.send_message(chat_id=CHAT_ID, text="Running...")
+    time.sleep(3600)
 
