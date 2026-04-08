0e6d80fe5337772b728a23464ee2c2a702a3ad86
Gerald <ezemagerald941@gmail.com>
2026-04-08 06:20:54 +0100

    Update message format for transaction notification

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def main():
                     
                     if current_tx != last_tx:
                         if last_tx is not None:
-                            await bot.send_message(CHAT_ID,  f"🚨 New Trx\n\nFrom:\n`{WALLET}`",
+                            await bot.send_message(CHAT_ID,  f"New Trx\n\nFrom:\n`{WALLET}`",
     parse_mode="Markdown")
                         last_tx = current_tx
             except Exception as e:
